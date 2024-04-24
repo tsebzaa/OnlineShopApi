a476851c2fc23c3ba301ee489c3b823525192b8c
tsebzaa <adrian_wypych@rossmann.com.pl>
2024-04-24 15:23:59 +0200

    a

--- OnlineShopApi/Program.cs
+++ OnlineShopApi/Program.cs
@@ -1,15 +1,21 @@
+using Application;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DevSafeRossContext>();
+
+builder.Services.AddScoped<InterfaceProductService, ProductService>();
+builder.Services.AddScoped<InterfaceProductRepository, ProductRepository>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
